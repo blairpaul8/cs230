b37e6b6e72837dc4407db32b9fea3523e32d90c5
Paul Blair <blairpaul8@yahoo.com>
2024-10-10 19:31:19 -0400

    added map.cpp

---
+++ inclass/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+ /* float value = 17.25;
+  unsigned int *i = (unsigned int *)&value;
+
+  cout << hex << *i << endl;
+  */
+
+  unsigned long value = 0xc040e0000000000000000UL;
+  double *dval = 
+}
